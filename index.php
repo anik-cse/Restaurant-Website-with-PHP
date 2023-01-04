942ae61616a0f40ef6edb9acf675506ba7f8cb7a
Mir Monoarul Alam <14123130+anik-cse@users.noreply.github.com>
2023-01-05 00:02:27 +0600

    Init

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include 'header.php'; ?>
+<!-- ======= Hero Section ======= -->
+<?php include 'herosection.php'; ?>
+<!-- End Hero Section -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <!-- End About Section -->
+    <?php include 'aboutus.php'; ?>
+    <!-- ======= Why Us Section ======= -->
+    <?php include 'whyus.php' ?>
+    <!-- End Why Box -->
+    <?php include 'whyus.php' ?>
+    <!-- ======= Stats Counter Section ======= -->
+    <?php include 'counter.php' ?>
+    <!-- End Stats Counter Section -->
+    <!-- ======= Menu Section ======= -->
+    <?php include 'menu.php' ?>
+    <!-- ======= Testimonials Section ======= -->
+    <?php include 'testimonial.php' ?>
+    <!-- End Testimonials Section -->
+
+    <!-- ======= Events Section ======= -->
+    <?php include 'event.php' ?>
+    <!-- End Events Section -->
+
+    <!-- ======= Chefs Section ======= -->
+    <?php include 'chefs.php' ?>
+    <!-- End Chefs Section -->
+
+    <!-- ======= Book A Table Section ======= -->
+    <?php include 'table.php' ?>
+    <!-- End Book A Table Section -->
+
+    <!-- ======= Gallery Section ======= -->
+    <?php include 'gallery.php' ?>
+    <!-- End Gallery Section -->
+    <?php include 'gallery.php' ?>
+    <!-- ======= Contact Section ======= -->
+    <?php include 'contact.php' ?>
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+<?php include 'footer.php' ?>
